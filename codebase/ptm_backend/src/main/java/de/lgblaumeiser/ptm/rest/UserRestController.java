d3a80ca4713abda622e0f35de27c4f34db3f0cfb
Lars Geyer-Blaumeiser <lgblaumeiser@gmail.com>
2019-02-10 18:58:38 +0100

    #112, #120: Add rest endpoint for change password and delete user and his data
    Signed-off-by: Lars Geyer-Blaumeiser <lgblaumeiser@gmail.com>

--- codebase/ptm_backend/src/main/java/de/lgblaumeiser/ptm/rest/UserRestController.java
+++ codebase/ptm_backend/src/main/java/de/lgblaumeiser/ptm/rest/UserRestController.java
@@ -11,6 +11,9 @@ import static de.lgblaumeiser.ptm.datamanager.model.User.newUser;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 import java.net.URI;
+import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +27,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import de.lgblaumeiser.ptm.ServiceMapper;
+import de.lgblaumeiser.ptm.datamanager.model.Activity;
+import de.lgblaumeiser.ptm.datamanager.model.Booking;
 import de.lgblaumeiser.ptm.datamanager.model.User;
 
 @RestController
@@ -54,12 +59,50 @@ public class UserRestController {
 		return ResponseEntity.created(location).build();
 	}
 
+	@RequestMapping(method = RequestMethod.POST, value = "/name")
+	ResponseEntity<?> changePassword(final Principal principal, @RequestBody final UserBody userData) {
+		logger.info("Request: Change Password for User " + principal.getName());
+		checkState(principal.getName().equals(userData.username));
+		User oldUser = services.userStore().retrieveAll().stream()
+				.filter(u -> u.getUsername().equals(principal.getName())).findFirst()
+				.orElseThrow(IllegalStateException::new);
+		services.userStore().store(oldUser.changeUser().setPassword(encrypt(userData.password)).build());
+		logger.info("Result: Passoword changed for User");
+		return ResponseEntity.ok().build();
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/name")
+	public ResponseEntity<?> deleteUserAndHisData(final Principal principal) {
+		logger.info("Request: Delete User and all his data with username " + principal.getName());
+		User user = services.userStore().retrieveAll().stream().filter(u -> u.getUsername().equals(principal.getName()))
+				.findFirst().orElseThrow(IllegalStateException::new);
+		List<Booking> relatedBookings = services.bookingStore().retrieveAll().stream()
+				.filter(b -> b.getUser().equals(principal.getName())).collect(Collectors.toList());
+		for (Booking booking : relatedBookings) {
+			services.bookingStore().deleteById(booking.getId());
+		}
+		List<Activity> relatedActivities = services.activityStore().retrieveAll().stream()
+				.filter(a -> a.getUser().equals(principal.getName())).collect(Collectors.toList());
+		for (Activity activity : relatedActivities) {
+			services.activityStore().deleteById(activity.getId());
+		}
+		services.userStore().deleteById(user.getId());
+		logger.info("Result: User and all his data deleted");
+		return ResponseEntity.ok().build();
+	}
+
 	private String encrypt(String password) {
 		return services.passwordEncodingService().encode(password);
 	}
 
 	private void checkUsername(String username) {
 		if (services.userStore().retrieveAll().stream().anyMatch(u -> u.getUsername().equals(username))) {
+			throw new IllegalStateException();
+		}
+	}
+
+	private void checkState(boolean state) {
+		if (!state) {
 			throw new IllegalStateException();
 		}
 	}
