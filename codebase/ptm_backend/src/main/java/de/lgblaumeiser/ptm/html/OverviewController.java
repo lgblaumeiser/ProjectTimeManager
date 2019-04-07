ef7f38420d3fb39d7bae286ac7b9117831f5f5e3
Lars Geyer-Blaumeiser <lars@lgblaumeiser.de>
2019-04-07 14:30:28 +0200

    #152: Fix activity sorting
    Signed-off-by: Lars Geyer-Blaumeiser <lars@lgblaumeiser.de>

--- codebase/ptm_backend/src/main/java/de/lgblaumeiser/ptm/html/OverviewController.java
+++ codebase/ptm_backend/src/main/java/de/lgblaumeiser/ptm/html/OverviewController.java
@@ -32,95 +32,106 @@ import de.lgblaumeiser.ptm.util.Utils;
 @Controller
 @RequestMapping("/overview")
 public class OverviewController {
-	private static final String TEMPLATENAME = "overview";
-	private static final String HOURANALYSISID = "HOURS";
-	private static final String ACTIVITIESANALYSISID = "ACTIVITIES";
-	private static final String PROJECTSANALYSISID = "PROJECTS";
-	private static final String MONTHTIMEFRAME = "month";
-	private static final String DAYTIMEFRAME = "day";
+    private static final String TEMPLATENAME = "overview";
+    private static final String HOURANALYSISID = "HOURS";
+    private static final String ACTIVITIESANALYSISID = "ACTIVITIES";
+    private static final String PROJECTSANALYSISID = "PROJECTS";
+    private static final String MONTHTIMEFRAME = "month";
+    private static final String DAYTIMEFRAME = "day";
 
-	private static final String DATEATTRIBUTE = "date";
-	private static final String MONTHATTRIBUTE = "month";
-	private static final String ALLACTIVITIESATTRIBUTE = "allActivities";
-	private static final String BOOKINGSFORDAYATTRIBUTE = "bookingsForDay";
-	private static final String HOURSANALYSISATTRIBUTE = "hourAnalysis";
-	private static final String HOURSANALYSISHEADLINEATTRIBUTE = "hourAnalysisHeadline";
-	private static final String ACTIVITIESANALYSISTODAYATTRIBUTE = "activityAnalysisToday";
-	private static final String ACTIVITIESANALYSISTODAYHEADLINEATTRIBUTE = "activityAnalysisTodayHeadline";
-	private static final String ACTIVITYANALYSISMONTHATTRIBUTE = "activityAnalysisMonth";
-	private static final String ACTIVITYANALYSISMONTHHEADLINEATTRIBUTE = "activityAnalysisMonthHeadline";
-	private static final String PROJECTANALYSISMONTHATTRIBUTE = "projectAnalysisMonth";
-	private static final String PROJECTANALYSISMONTHHEADLINEATTRIBUTE = "projectAnalysisMonthHeadline";
+    private static final String DATEATTRIBUTE = "date";
+    private static final String MONTHATTRIBUTE = "month";
+    private static final String ALLACTIVITIESATTRIBUTE = "allActivities";
+    private static final String BOOKINGSFORDAYATTRIBUTE = "bookingsForDay";
+    private static final String HOURSANALYSISATTRIBUTE = "hourAnalysis";
+    private static final String HOURSANALYSISHEADLINEATTRIBUTE = "hourAnalysisHeadline";
+    private static final String ACTIVITIESANALYSISTODAYATTRIBUTE = "activityAnalysisToday";
+    private static final String ACTIVITIESANALYSISTODAYHEADLINEATTRIBUTE = "activityAnalysisTodayHeadline";
+    private static final String ACTIVITYANALYSISMONTHATTRIBUTE = "activityAnalysisMonth";
+    private static final String ACTIVITYANALYSISMONTHHEADLINEATTRIBUTE = "activityAnalysisMonthHeadline";
+    private static final String PROJECTANALYSISMONTHATTRIBUTE = "projectAnalysisMonth";
+    private static final String PROJECTANALYSISMONTHHEADLINEATTRIBUTE = "projectAnalysisMonthHeadline";
 
-	@Autowired
-	private ServiceMapper services;
+    @Autowired
+    private ServiceMapper services;
 
-	static class BookingStruct {
-		public Long id;
-		public String starttime;
-		public String endtime;
-		public Long activity;
-		public String comment;
+    static class BookingStruct {
+        public Long id;
+        public String starttime;
+        public String endtime;
+        public Long activity;
+        public String comment;
 
-		BookingStruct(final Long id, final String starttime, final String endtime, final Long activity,
-				final String comment) {
-			this.id = id;
-			this.starttime = starttime;
-			this.endtime = endtime;
-			this.activity = activity;
-			this.comment = comment;
-		}
-	}
+        BookingStruct(final Long id, final String starttime, final String endtime, final Long activity,
+                final String comment) {
+            this.id = id;
+            this.starttime = starttime;
+            this.endtime = endtime;
+            this.activity = activity;
+            this.comment = comment;
+        }
+    }
 
-	@RequestMapping(method = RequestMethod.GET)
-	public String dataPageForToday(final Principal principal, final Model model) {
-		return createPage(model, principal.getName(), LocalDate.now());
-	}
+    @RequestMapping(method = RequestMethod.GET)
+    public String dataPageForToday(final Principal principal, final Model model) {
+        return createPage(model, principal.getName(), LocalDate.now());
+    }
 
-	@RequestMapping(method = RequestMethod.GET, value = "/{dayString}")
-	public String dataPageForDay(final Principal principal, @PathVariable final String dayString, final Model model) {
-		return createPage(model, principal.getName(), LocalDate.parse(dayString));
-	}
+    @RequestMapping(method = RequestMethod.GET, value = "/{dayString}")
+    public String dataPageForDay(final Principal principal, @PathVariable final String dayString, final Model model) {
+        return createPage(model, principal.getName(), LocalDate.parse(dayString));
+    }
 
-	private String createPage(final Model model, final String username, final LocalDate dateToShow) {
-		model.addAttribute(DATEATTRIBUTE, dateToShow.format(DateTimeFormatter.ISO_LOCAL_DATE));
-		model.addAttribute(MONTHATTRIBUTE, dateToShow.format(DateTimeFormatter.ofPattern("yyyy-MM")));
-		model.addAttribute(ALLACTIVITIESATTRIBUTE,
-				services.activityStore().retrieveAll().stream()
-						.filter(act -> !act.isHidden() && act.getUser().equals(username))
-						.sorted((a1, a2) -> a1.getProjectActivity().compareToIgnoreCase(a2.getProjectActivity()))
-						.collect(toList()));
-		model.addAttribute(BOOKINGSFORDAYATTRIBUTE, services.bookingStore().retrieveAll().stream()
-				.filter(b -> b.getBookingday().equals(dateToShow) && b.getUser().equals(username))
-				.sorted(Comparator.comparing(Booking::getStarttime))
-				.map(b -> new BookingStruct(b.getId(), b.getStarttime().format(DateTimeFormatter.ofPattern("HH:mm")),
-						b.hasEndtime() ? b.getEndtime().format(DateTimeFormatter.ofPattern("HH:mm")) : "",
-						b.getActivity(), b.getComment()))
-				.collect(toList()));
+    private String createPage(final Model model, final String username, final LocalDate dateToShow) {
+        model.addAttribute(DATEATTRIBUTE, dateToShow.format(DateTimeFormatter.ISO_LOCAL_DATE));
+        model.addAttribute(MONTHATTRIBUTE, dateToShow.format(DateTimeFormatter.ofPattern("yyyy-MM")));
+        model.addAttribute(ALLACTIVITIESATTRIBUTE,
+                services
+                        .activityStore()
+                        .retrieveAll()
+                        .stream()
+                        .filter(act -> !act.isHidden() && act.getUser().equals(username))
+                        .sorted((a1, a2) -> a1.getProjectId().compareToIgnoreCase(a2.getProjectId()))
+                        .collect(toList()));
+        model.addAttribute(BOOKINGSFORDAYATTRIBUTE,
+                services
+                        .bookingStore()
+                        .retrieveAll()
+                        .stream()
+                        .filter(b -> b.getBookingday().equals(dateToShow) && b.getUser().equals(username))
+                        .sorted(Comparator.comparing(Booking::getStarttime))
+                        .map(b -> new BookingStruct(
+                                b.getId(),
+                                b.getStarttime().format(DateTimeFormatter.ofPattern("HH:mm")),
+                                b.hasEndtime() ? b.getEndtime().format(DateTimeFormatter.ofPattern("HH:mm")) : "",
+                                b.getActivity(),
+                                b.getComment()))
+                        .collect(toList()));
 
-		setAnalysisData(model, HOURSANALYSISHEADLINEATTRIBUTE, HOURSANALYSISATTRIBUTE, HOURANALYSISID, MONTHTIMEFRAME,
-				dateToShow.format(DateTimeFormatter.ofPattern("yyyy-MM")), username);
+        setAnalysisData(model, HOURSANALYSISHEADLINEATTRIBUTE, HOURSANALYSISATTRIBUTE, HOURANALYSISID, MONTHTIMEFRAME,
+                dateToShow.format(DateTimeFormatter.ofPattern("yyyy-MM")), username);
 
-		setAnalysisData(model, ACTIVITIESANALYSISTODAYHEADLINEATTRIBUTE, ACTIVITIESANALYSISTODAYATTRIBUTE,
-				ACTIVITIESANALYSISID, DAYTIMEFRAME, dateToShow.format(DateTimeFormatter.ISO_LOCAL_DATE), username);
+        setAnalysisData(model, ACTIVITIESANALYSISTODAYHEADLINEATTRIBUTE, ACTIVITIESANALYSISTODAYATTRIBUTE,
+                ACTIVITIESANALYSISID, DAYTIMEFRAME, dateToShow.format(DateTimeFormatter.ISO_LOCAL_DATE), username);
 
-		setAnalysisData(model, ACTIVITYANALYSISMONTHHEADLINEATTRIBUTE, ACTIVITYANALYSISMONTHATTRIBUTE,
-				ACTIVITIESANALYSISID, MONTHTIMEFRAME, dateToShow.format(DateTimeFormatter.ofPattern("yyyy-MM")),
-				username);
+        setAnalysisData(model, ACTIVITYANALYSISMONTHHEADLINEATTRIBUTE, ACTIVITYANALYSISMONTHATTRIBUTE,
+                ACTIVITIESANALYSISID, MONTHTIMEFRAME, dateToShow.format(DateTimeFormatter.ofPattern("yyyy-MM")),
+                username);
 
-		setAnalysisData(model, PROJECTANALYSISMONTHHEADLINEATTRIBUTE, PROJECTANALYSISMONTHATTRIBUTE, PROJECTSANALYSISID,
-				MONTHTIMEFRAME, dateToShow.format(DateTimeFormatter.ofPattern("yyyy-MM")), username);
+        setAnalysisData(model, PROJECTANALYSISMONTHHEADLINEATTRIBUTE, PROJECTANALYSISMONTHATTRIBUTE, PROJECTSANALYSISID,
+                MONTHTIMEFRAME, dateToShow.format(DateTimeFormatter.ofPattern("yyyy-MM")), username);
 
-		return TEMPLATENAME;
-	}
+        return TEMPLATENAME;
+    }
 
-	private void setAnalysisData(final Model model, final String headlineAttr, final String analysisAttr,
-			final String analysisId, final String timeFrameType, final String timeFrame, final String username) {
-		Collection<Collection<String>> analysisResult = services.analysisService().analyze(analysisId, username,
-				timeFrameType, timeFrame);
-		Collection<String> headline = Utils.getFirstFromCollection(analysisResult);
-		Collection<Collection<String>> bodydata = analysisResult.stream().skip(1).collect(toList());
-		model.addAttribute(headlineAttr, headline);
-		model.addAttribute(analysisAttr, bodydata);
-	}
+    private void setAnalysisData(final Model model, final String headlineAttr, final String analysisAttr,
+            final String analysisId, final String timeFrameType, final String timeFrame, final String username) {
+        Collection<Collection<String>> analysisResult = services
+                .analysisService()
+                .analyze(analysisId, username, timeFrameType, timeFrame);
+        Collection<String> headline = Utils.getFirstFromCollection(analysisResult);
+        Collection<Collection<String>> bodydata = analysisResult.stream().skip(1).collect(toList());
+        model.addAttribute(headlineAttr, headline);
+        model.addAttribute(analysisAttr, bodydata);
+    }
 }
