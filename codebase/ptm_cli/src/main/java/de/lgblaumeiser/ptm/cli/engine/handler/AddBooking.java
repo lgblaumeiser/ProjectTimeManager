fdbb2bfcc408d370ea33280b4d827ec9cab47601
Lars <lars@lgblaumeiser.com>
2020-10-03 15:41:06 +0200

    Fix sca issue although false positive
    Signed-off-by: Lars <lars@lgblaumeiser.com>

--- codebase/ptm_cli/src/main/java/de/lgblaumeiser/ptm/cli/engine/handler/AddBooking.java
+++ codebase/ptm_cli/src/main/java/de/lgblaumeiser/ptm/cli/engine/handler/AddBooking.java
@@ -23,35 +23,36 @@ import de.lgblaumeiser.ptm.datamanager.model.Booking.BookingBuilder;
  */
 @Parameters(commandDescription = "Add a new booking")
 public class AddBooking extends AbstractCommandHandler {
-	private final static String DUMMYUSER = "Dummy";
+    private final static String DUMMYUSER = "Dummy";
 
-	@Parameter(names = { "-d",
-			"--day" }, description = "Optional day for booking, either a iso date format or -<days>", converter = LocalDateConverter.class)
-	private LocalDate bookingDay = LocalDate.now();
+    @Parameter(names = { "-d",
+            "--day" }, description = "Optional day for booking, either a iso date format or -<days>", converter = LocalDateConverter.class)
+    private LocalDate bookingDay = LocalDate.now();
 
-	@Parameter(names = { "-a", "--activity" }, description = "Activity id of the bookings activity", required = true)
-	private Long activityId;
+    @Parameter(names = { "-a", "--activity" }, description = "Activity id of the bookings activity", required = true)
+    private Long activityId;
 
-	@Parameter(names = { "-s",
-			"--starttime" }, description = "Start time of the booked time frame", required = true, converter = LocalTimeConverter.class)
-	private Optional<LocalTime> starttime;
+    @Parameter(names = { "-s",
+            "--starttime" }, description = "Start time of the booked time frame", required = true, converter = LocalTimeConverter.class)
+    private Optional<LocalTime> starttime;
 
-	@Parameter(names = { "-e",
-			"--endtime" }, description = "End time of the booked time frame", converter = LocalTimeConverter.class)
-	private Optional<LocalTime> endtime = Optional.empty();
+    @Parameter(names = { "-e",
+            "--endtime" }, description = "End time of the booked time frame", converter = LocalTimeConverter.class)
+    private Optional<LocalTime> endtime = Optional.empty();
 
-	@Parameter(names = { "-c",
-			"--comment" }, description = "Comment on booked time frame", converter = OptionalStringConverter.class)
-	private Optional<String> comment = Optional.empty();
+    @Parameter(names = { "-c",
+            "--comment" }, description = "Comment on booked time frame", converter = OptionalStringConverter.class)
+    private Optional<String> comment = Optional.empty();
 
-	@Override
-	public void handleCommand() {
-		getLogger().log("Add new booking ...");
-		BookingBuilder newBooking = Booking.newBooking().setBookingday(bookingDay).setActivity(activityId)
-				.setStarttime(starttime.get()).setUser(DUMMYUSER);
-		endtime.ifPresent(newBooking::setEndtime);
-		comment.ifPresent(newBooking::setComment);
-		Booking addedBooking = getServices().getBookingsStore().store(newBooking.build());
-		getLogger().log(" ... booking added with data: " + addedBooking.toString());
-	}
+    @Override
+    public void handleCommand() {
+        getLogger().log("Add new booking ...");
+        BookingBuilder newBooking = Booking.newBooking().setBookingday(bookingDay).setActivity(activityId)
+                .setUser(DUMMYUSER);
+        starttime.ifPresent(newBooking::setStarttime);
+        endtime.ifPresent(newBooking::setEndtime);
+        comment.ifPresent(newBooking::setComment);
+        Booking addedBooking = getServices().getBookingsStore().store(newBooking.build());
+        getLogger().log(" ... booking added with data: " + addedBooking.toString());
+    }
 }
